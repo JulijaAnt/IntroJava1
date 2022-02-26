a55f83b8312ae3da0977f16cb2df1638383b7c68
JulijaAnt <julija.antoscenko@inbox.lv>
2022-02-26 19:05:31 +0200

    thank you

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("Hello now");
+    int number;
+    number = 8;
+    int value =6;
+    System.out.println("Sum of number plus value is: " + (number + value));
+    System.out.println("deduct number minus value is: " + (number - value));
+    System.out.println("Multiply number and value is: " + (number * value));
+    }
+}
