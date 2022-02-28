a5ce82ae8c7453a99627ef1960c7b41c2cc01a85
JulijaAnt <julija.antoscenko@inbox.lv>
2022-02-28 18:53:22 +0200

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Hello now");
+	System.out.println("Hello now");
     int number;
-    number = 8;
-    int value =6;
+    number = 11;
+    float value =8;
     System.out.println("Sum of number plus value is: " + (number + value));
     System.out.println("deduct number minus value is: " + (number - value));
     System.out.println("Multiply number and value is: " + (number * value));
+    System.out.println("Division number and value is: " + (number / value));
+    System.out.println("Division number and value is: " + (number / value));
     }
 }
