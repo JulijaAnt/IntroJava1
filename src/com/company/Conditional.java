5ff6dfb83b0b87d040a9d4108033d087048aaed1
JulijaAnt <julija.antoscenko@inbox.lv>
2022-03-01 16:34:20 +0200

    changes

--- src/com/company/Conditional.java
+++ src/com/company/Conditional.java
@@ -2,8 +2,14 @@ package com.company;
 
 public class Conditional {
     public static void main(String[] args) {
-    int number = 10;
+    int number = 15;
     if ( number > 9 && number < 14){
+            System.out.println("10,11,12,13,14");
+    } else {
+            System.out.println("not ok");
+    }
+    int Newnumber = 12;
+    if ( number > 9 ){
             System.out.println("10,11,12,13,14");
     } else {
             System.out.println("not ok");
